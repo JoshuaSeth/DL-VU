fab9cc467aae26d1e6cab6d7ec733f74b557dd75
AlexHoorn <12898097+AlexHoorn@users.noreply.github.com>
2022-12-09 00:11:34 +0100

    Q3 Elman

--- Alex_temp/q3.py
+++ Alex_temp/q3.py
@@ -1,11 +1,13 @@
-from torch import nn
 import torch
+from torch import nn
+from utils import DEVICE, GlobalMaxPool
+
 
 class Elman(nn.Module):
-    def __init__(self, insize=300, outsize=300, hsize=300):
+    def __init__(self, insize=300, outsize=300, hsize=300, activation=nn.ReLU):
         super().__init__()
 
-        self.lin1 = nn.Linear(insize + hsize, hsize)
+        self.lin1 = nn.Sequential(nn.Linear(insize + hsize, hsize), activation())
         self.lin2 = nn.Linear(hsize, outsize)
 
     def forward(self, x, hidden=None):
@@ -14,14 +16,15 @@ class Elman(nn.Module):
         # Initialize hidden with zeros if not given
         if hidden is None:
             hidden = torch.zeros(b, e, dtype=torch.float)
+            hidden = hidden.to(DEVICE)
 
         outs = []
 
         for i in range(t):
             # Concat previous hidden layer with input
             inp = torch.cat([x[:, i, :], hidden], dim=1)
-            
-            # First layer is our new hidden
+
+            # First layer output is our new hidden
             hidden = self.lin1(inp)
             # Output layer
             out = self.lin2(hidden)
@@ -29,3 +32,27 @@ class Elman(nn.Module):
             outs.append(out[:, None, :])
 
         return torch.cat(outs, dim=1), hidden
+
+
+class RNN(nn.Module):
+    def __init__(
+        self, n_embeddings: int, embedding_size: int, hidden: int, num_classes: int
+    ) -> None:
+        super().__init__()
+
+        self.elman = nn.Sequential(
+            nn.Embedding(n_embeddings, embedding_size), Elman(embedding_size, hidden)
+        )
+
+        self.linear = nn.Sequential(
+            nn.ReLU(),
+            GlobalMaxPool(1),
+            nn.Linear(hidden, num_classes),
+            nn.Softmax(),
+        )
+
+    def forward(self, x):
+        out, _ = self.elman(x)
+        out = self.linear(out)
+
+        return out
---
+++ Alex_temp/utils.py
@@ -0,0 +1,75 @@
+import torch
+from torch import nn
+from torch.nn.utils.rnn import pad_sequence
+from torch.utils.data import Dataset
+
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+
+class GlobalMaxPool(nn.Module):
+    def __init__(self, dim: int) -> None:
+        super().__init__()
+        self.dim = dim
+
+    def forward(self, x):
+        return torch.max(x, dim=self.dim)[0]
+
+
+class IMDBDataset(Dataset):
+    def __init__(self, x, y, padding_value=0) -> None:
+        super().__init__()
+
+        x = [torch.tensor(xi) for xi in x]
+        self.x = pad_sequence(x, batch_first=True, padding_value=padding_value)
+
+        self.y = torch.tensor(y)
+
+    def __len__(self):
+        return len(self.y)
+
+    def __getitem__(self, index):
+        return self.x[index], self.y[index]
+
+
+def train_epoch(model, data_loader, optimizer, loss_func, device=DEVICE):
+    # Make sure to calculate gradients
+    model.train(True)
+    epoch_loss = 0.0
+
+    for x_batch, y_batch in data_loader:
+        x_batch = x_batch.to(device)
+        y_batch = y_batch.to(device)
+
+        # Forward & optim
+        optimizer.zero_grad()
+        loss = loss_func(model(x_batch), y_batch)
+        loss.backward()
+        optimizer.step()
+
+        epoch_loss += loss.item()
+
+    # Mean loss over batches
+    return epoch_loss / len(data_loader)
+
+
+def get_score(model, data_loader, loss_func, device=DEVICE):
+    # Don't need gradients here
+    model.train(False)
+    loss = 0.0
+    correct = []
+
+    for x_batch, y_batch in data_loader:
+        x_batch = x_batch.to(device)
+        y_batch = y_batch.to(device)
+
+        out = model.forward(x_batch)
+        loss += loss_func(out, y_batch).item()
+
+        # Keep track of correct predictions for accuracy
+        correct += (out.argmax(dim=1) == y_batch).tolist()
+
+    # Mean loss over batches
+    mean_loss = loss / len(data_loader)
+    accuracy = sum(correct) / len(correct)
+
+    return mean_loss, accuracy
